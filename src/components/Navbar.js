df838e629761e24c035f5ef40a3a21777767fbfc
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2022-01-03 12:17:19 +0000

    Clearing out pizza images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,9 @@ import logo from '../assets/devsci-logo.svg'
 function Navbar() {
     return (
         <div className='navbar'>
-            <div className='leftSide'></div>
-            <img align='left' src={logo} alt='Logo' width='50px' height='50px' />
+            <div className='leftSide'><img align='left' src={logo} alt='Logo' width='50px' height='50px' /></div>
+
+            
             <div className='rightSide'></div>
         </div>
     )
